3f578f9dc18592a03bc703377174e687d7ac9096
Nicolas Linden <linden.nicolas@orange.fr>
2024-08-06 15:50:06 +0200

    load

--- ndmanager/omcer/commands.py
+++ ndmanager/omcer/commands.py
@@ -1,17 +1,10 @@
 import argparse as ap
 import os
-from itertools import product
-from ndmanager.data import NSUB_list, ENDF6_PATH, OPENMC_NUCLEAR_DATA
-import numpy as np
-from tabulate import tabulate
-from multiprocessing import Pool
-import time
-from ndmanager.utils import clear_line, print_offset
+from ndmanager.data import OPENMC_NUCLEAR_DATA, NDM_DIR
 from ndmanager.fetcher.download import download
 from ndmanager.omcer.generate import chain, generate
 from ndmanager.omcer.substitute import replace_negatives_in_lib
 from ndmanager.omcer.download import download
-from ndmanager.data import NDLIBS
 import shutil
 
 def ndo_sn301(args: ap.Namespace):
@@ -62,7 +55,7 @@ def ndo_path(args: ap.Namespace):
 
 def ndo_load(args: ap.Namespace):
     target = OPENMC_NUCLEAR_DATA / args.library / "cross_sections.xml"
-    link = OPENMC_NUCLEAR_DATA / "cross_sections.xml"
+    link = NDM_DIR / "cross_sections.xml"
     if link.exists():
         link.unlink()
     link.symlink_to(target)
