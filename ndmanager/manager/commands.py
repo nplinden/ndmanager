e2079f2b09687a412e257c97a614e70b7afb9989
Nicolas Linden <linden.nicolas@orange.fr>
2024-08-06 15:35:15 +0200

    mince

--- ndmanager/manager/commands.py
+++ ndmanager/manager/commands.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 import argparse as ap
+import shutil
 
 def ndm_init(args: ap.Namespace):
     dotfile = Path.home() / ".ndmanager"
 
     if dotfile.exists():
         if args.force:
-            dotfile.unlink()
+            shutil.rmtree(dotfile)
         else:
             raise ValueError(f"{dotfile} directory already exists.")
     dotfile.mkdir(parents=True)
