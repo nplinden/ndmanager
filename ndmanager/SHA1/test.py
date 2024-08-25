034f0ffeb26a1d835860667d5643b82293fd1357
Nicolas Linden <57541749+nplinden@users.noreply.github.com>
2024-08-25 17:45:03 +0200

    formatting

--- ndmanager/SHA1/test.py
+++ ndmanager/SHA1/test.py
@@ -1,8 +1,9 @@
-TEST_SHA1 = {'test/tsl/tsl_0056_26-Fe-56': 'd6e6b0c7a55d2b52ffae63c644b9754eb7b1483f',
-'test/n/C12': 'af075e8479d0bc26bcbb4c99084f7d048dd41f0a',
-'test/n/Fe56': 'a657ae7e659057281e17f291f8a941ab6f243c74',
-'test/ard/Fe0': 'ae71fa874f9a86e3c96543c4133b679439ead240',
-'test/ard/C0': '25baa0fa7074046cf29cdcafe655d301238c4e06',
-'test/photo/Fe0': '4f95e91630c3805c5c823e351bb4cf3ae62ff475',
-'test/photo/C0': 'a384c9fb865a0a9f812ec84242b68d84c27b698c',
+TEST_SHA1 = {
+    "test/tsl/tsl_0056_26-Fe-56": "d6e6b0c7a55d2b52ffae63c644b9754eb7b1483f",
+    "test/n/C12": "af075e8479d0bc26bcbb4c99084f7d048dd41f0a",
+    "test/n/Fe56": "a657ae7e659057281e17f291f8a941ab6f243c74",
+    "test/ard/Fe0": "ae71fa874f9a86e3c96543c4133b679439ead240",
+    "test/ard/C0": "25baa0fa7074046cf29cdcafe655d301238c4e06",
+    "test/photo/Fe0": "4f95e91630c3805c5c823e351bb4cf3ae62ff475",
+    "test/photo/C0": "a384c9fb865a0a9f812ec84242b68d84c27b698c",
 }
