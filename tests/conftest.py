c2c113224dce83adf2bead284a1e165f853d68a9
Nicolas Linden <n.linde@naarea.fr>
2024-09-13 16:05:20 +0200

    add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,38 @@
 import pytest
 import argparse as ap
+from pathlib import Path
 from ndmanager.CLI.fetcher.install import install
+from ndmanager.CLI.omcer.build import build
 
 
 @pytest.fixture(scope="session")
 def install_test():
     namespace = ap.Namespace(libraries=["test"], sub=["n"], all=False)
     install(namespace)
+
+@pytest.fixture(scope="session")
+def build_test(install_test):
+    data = """description: |
+  A test library
+name: test
+n:
+  basis: test
+  ommit: Am242_m1
+tsl:
+  basis: test
+photo:
+  basis: test
+ard:
+  basis: test
+temperatures: 273 500
+"""
+    p = Path("pytest-artifacts/test.yml")
+    with open(p, "w") as f:
+        print(data, file=f)
+    namespace = ap.Namespace(
+        filename=str(p),
+        dryrun=False,
+        clean=False,
+        j=2
+    )
+    build(namespace)
