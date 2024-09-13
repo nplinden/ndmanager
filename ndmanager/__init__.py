c20d6631e98119980330288a53318b1cbbd38dd2
Nicolas Linden <n.linde@naarea.fr>
2024-09-13 16:02:20 +0200

    change init

--- ndmanager/__init__.py
+++ ndmanager/__init__.py
@@ -1,4 +1,5 @@
 """The NDManager module"""
 from .API.endf6 import Endf6
 from .API.nuclide import Nuclide
-from .API.utils import *
+from .API.sha1 import compute_file_sha1
+from .API.utils import download_endf6, get_endf6, fetch_sublibrary_list
