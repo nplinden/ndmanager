0b0310f52e8b760a044865069b40377e12bb2ff1
Nicolas Linden <linden.nicolas@orange.fr>
2024-09-14 21:03:24 +0200

    fix IAEA

--- ndmanager/__init__.py
+++ ndmanager/__init__.py
@@ -3,4 +3,5 @@
 from .API.endf6 import Endf6
 from .API.nuclide import Nuclide
 from .API.sha1 import compute_file_sha1
-from .API.utils import download_endf6, fetch_sublibrary_list, get_endf6
+from .API.utils import get_endf6
+from .API.iaea import download_endf6, fetch_sublibrary_list
