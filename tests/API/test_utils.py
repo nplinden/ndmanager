d93999e2bc51b96f81baca28b5c805b7b6b40d9a
Nicolas Linden <linden.nicolas@orange.fr>
2024-09-14 22:43:46 +0200

    adding tests

--- tests/API/test_utils.py
+++ tests/API/test_utils.py
@@ -4,3 +4,9 @@ from ndmanager.API.utils import get_endf6
 
 def test_get_endf6(install_test):
     get_endf6("test", "n", "C12").samefile("pytest-artifacts/endf6/test/n/C12.endf6")
+    with pytest.raises(ValueError):
+        get_endf6("coucou", "n", "C12")
+    with pytest.raises(ValueError):
+        get_endf6("test", "coucou", "C12")
+    with pytest.raises(ValueError):
+        get_endf6("test", "n", "coucou")
