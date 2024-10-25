f4ea04ecd502457cdebf38eb827f10289e845904
Nicolas Linden <n.linde@naarea.fr>
2024-10-25 17:29:03 +0200

    better

--- ndmanager/API/sampling/sampling.py
+++ ndmanager/API/sampling/sampling.py
@@ -3,16 +3,28 @@ import logging
 import shutil
 import tempfile
 from contextlib import chdir
-import shlex
+import multiprocessing as mp
 from pathlib import Path
 from tqdm import tqdm
+from sandy.endf6 import Endf6
+import subprocess as sp
+import os
+from collections import namedtuple
 
 import openmc.data
 from openmc.data import DataLibrary
+from sandy.samples import Samples
+from sandy.utils import get_seed
 from sandy.sampling import run
 from ndmanager.env import NDMANAGER_SAMPLES, NDMANAGER_HDF5
 from ndmanager import get_endf6
 
+SampleTapes = namedtuple("SampleTapes", ["nuclide", "xs_lib", "matrix_lib"])
+
+def ace_to_hdf5(ace, target):
+    neutron = openmc.data.IncidentNeutron.from_ace(ace)
+    _, pertid = ace.name.split(".")[0].split("_")
+    neutron.export_to_hdf5(target / f"{pertid}.h5", "w")
 class Sampling:
     def __init__(self, yaml_path):
         input_dict = yaml.safe_load(open(yaml_path, "r"))
@@ -20,11 +32,18 @@ class Sampling:
         self.name = input_dict["name"]
         self.reuse = input_dict["reuse"]
         self.temperature = input_dict["temperature"]
+        self.seed31 = get_seed()
+        self.seed33 = get_seed()
+        self.seed34 = get_seed()
+        self.seed35 = get_seed()
 
-        self.lib_nuc_couples = []
-        for library, nuclides in input_dict["samples"].items():
-            for nuclide in nuclides.split():
-                self.lib_nuc_couples.append((library, nuclide))
+        self.tapes = []
+        for nuclide, libraries_ in input_dict["samples"].items():
+            libraries = libraries_.split()
+            if len(libraries) == 1:
+                self.tapes.append(SampleTapes(nuclide, libraries[0], libraries[0]))
+            else:
+                self.tapes.append(SampleTapes(nuclide, libraries[0], libraries[1]))
 
         self.rootpath = NDMANAGER_SAMPLES / self.name
         self.xs_path = self.rootpath / "cross_sections"
@@ -41,10 +60,10 @@ class Sampling:
     def sample(self, processes):
         bar_format = "{l_bar}{bar:40}| {n_fmt}/{total_fmt} [{elapsed}s]"
         pbar = tqdm(
-                total=len(self.lib_nuc_couples),
+                total=len(self.tapes),
                 bar_format=bar_format,
             )
-        for library, nuclide in self.lib_nuc_couples:
+        for nuclide, xs_lib, matrix_lib in self.tapes:
             pbar.set_description(f"Sampling {nuclide:8s}")
             target = self.rootpath / nuclide
             target.mkdir()
@@ -53,13 +72,12 @@ class Sampling:
                                 format= "%(asctime)s [%(levelname)s]: %(message)s",
                                 datefmt= "%Y-%m-%d %H:%M:%S",
                                 force=True)
-            tape = get_endf6(library, "n", nuclide)
+            xs_file = get_endf6(xs_lib, "n", nuclide)
+            matrix_file = get_endf6(matrix_lib, "n", nuclide)
 
             with tempfile.TemporaryDirectory() as tmpdir:
                 with chdir(tmpdir):
-                    command = (f"%s --mf 33 --samples %d --processes %d --temperatures %d"
-                                " --supressnjoy  --acer ")
-                    run(shlex.split(command % (tape, self.nsmp, processes, self.temperature)))
+                    self.run(xs_file, matrix_file, processes)
                     
                     for xsd in Path(".").glob("*.xsd"):
                         xsd.unlink()
@@ -68,10 +86,14 @@ class Sampling:
                     for xslx in Path(".").glob("*.xlsx"):
                         shutil.move(xslx, target)
 
-                    for ace in Path(".").glob("*"):
-                        neutron = openmc.data.IncidentNeutron.from_ace(ace)
-                        _, pertid = ace.name.split(".")[0].split("_")
-                        neutron.export_to_hdf5(target / f"{pertid}.h5", "w")
+                    with mp.get_context("spawn").Pool(processes) as p:
+                        for ace in Path(".").glob("*"):
+                            p.apply_async(
+                                ace_to_hdf5,
+                                args=(ace, target)
+                            )
+                        p.close()
+                        p.join()
             pbar.update()
         pbar.close()
         
@@ -79,9 +101,76 @@ class Sampling:
 
         for ismp in range(self.nsmp):
             library = DataLibrary.from_xml(reuse_xml)
-            for _, nuclide in self.lib_nuc_couples:
+            for nuclide, _, _ in self.tapes:
                 h5path = self.rootpath / nuclide / f"{ismp}.h5"
                 if h5path.exists():
                     library.remove_by_material(nuclide)
                     library.register_file(h5path)
             library.export_to_xml(self.rootpath / f"cross_sections/{ismp}.xml")
+
+    def run(self, xs_file, matrix_file, processes):
+        logging.getLogger().setLevel(logging.DEBUG)
+
+        err_pendf = 0.01
+        err_ace = 0.01
+        err_errorr = 0.1
+
+        njoy_output = sp.DEVNULL
+
+        # ERRORR KEYWORDS
+        errorr_kws = dict(
+            verbose=False,
+            err=err_errorr,
+            xs=True,
+            nubar=False,
+            chi=False,
+            mubar=False,
+            groupr_kws=dict(nubar=False, chi=False, mubar=False, ign=2),
+            errorr_kws=dict(ign=2),
+            njoy_output=njoy_output,
+            errorr33_kws = dict(mt=None)
+        )
+
+        smp_kws = {
+            "seed31": self.seed31,
+            "seed33": self.seed33,
+            "seed34": self.seed34,
+            "seed35": self.seed35,
+        }
+
+        matrix_tape = Endf6.from_file(matrix_file)
+        logging.info(f"Running ERRORR on: '{matrix_file}'")
+        smps = matrix_tape.get_perturbations(self.nsmp, njoy_kws=errorr_kws, smp_kws=smp_kws)
+
+        # PENDF KEYWORDS
+        pendf_kws = dict(
+            verbose=False,
+            err=err_pendf,
+            minimal_processing=False,
+            njoy_output=njoy_output,
+            )
+
+        # ACE KEYWORDS
+        ace_kws = dict(
+            verbose=False,
+            err=err_ace,
+            minimal_processing=False,
+            temperature=self.temperature,
+            purr=False,
+            njoy_output=njoy_output,
+            )
+
+        xs_tape = Endf6.from_file(xs_file) 
+        logging.info(f"Applying perturbations on: '{matrix_file}'")
+        xs_tape.apply_perturbations(
+            smps,
+            processes=processes,
+            to_file=True,
+            to_ace=True,
+            filename="{ZA}_{SMP}",
+            njoy_kws=pendf_kws,
+            ace_kws=ace_kws,
+            verbose=False
+        )
+
+        return
