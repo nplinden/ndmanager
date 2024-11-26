ff86114fd9dac94fac96a10cffa5e8e233e2d533
Nicolas Linden <n.linde@naarea.fr>
2024-11-26 14:27:07 +0100

    oups

--- ndmanager/CLI/omcer/listlibs.py
+++ ndmanager/CLI/omcer/listlibs.py
@@ -48,7 +48,7 @@ def listlibs(_args):
             lst.append("\n".join(s))
     lst.append(header("Custom Libraries"))
     for name in xs:
-        with open(NDMANAGER_HDF5 / name / "input.yml", "r", encoding="ytf-8") as f:
+        with open(NDMANAGER_HDF5 / name / "input.yml", "r", encoding="utf-8") as f:
             desc = yaml.safe_load(f).get("summary", "")
         s = f"{name:<16} {desc}"
         s = textwrap.wrap(s, initial_indent="", subsequent_indent=21 * " ", width=col)
