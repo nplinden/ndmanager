75898efb63e9a9518794f0e3148454db4357fa02
Nicolas Linden <n.linde@naarea.fr>
2024-11-27 08:40:49 +0100

    some more tests

--- tests/CLI/test_ndo.py
+++ tests/CLI/test_ndo.py
@@ -3,12 +3,14 @@ import shlex
 import pytest
 from ndmanager.CLI.omcer.main import parser
 
+def run(command):
+    args = parser.parse_args(shlex.split(command))
+    args.func(args)
+
 def test_ndo_list_install_remove(capsys):
     p = Path("pytest/hdf5")
 
-    command = "list"
-    args = parser.parse_args(shlex.split(command))
-    args.func(args)
+    run("list")
     captured = capsys.readouterr()
     expected = ("-------------------------------------------------------------"
     "-  Installable Libraries  -----------------------------------------------"
@@ -25,13 +27,9 @@ def test_ndo_list_install_remove(capsys):
     "------------------------------------------------------------\n")
     assert captured.out == expected
 
-    command = "install lanl/endfb70"
-    args = parser.parse_args(shlex.split(command))
-    args.func(args)
+    run("install lanl/endfb70")
 
-    command = "list"
-    args = parser.parse_args(shlex.split(command))
-    args.func(args)
+    run("list")
     captured = capsys.readouterr()
     expected = ("-------------------------------------------------------------"
     "-  Installable Libraries  -----------------------------------------------"
@@ -40,6 +38,24 @@ def test_ndo_list_install_remove(capsys):
     "ial OpenMC library based on ENDF-B/VIII.0\nofficial/jeff33  JEFF-3.3     "
     "   [ ]: Official OpenMC library based on JEFF-3.3\nlanl/endfb70     ENDF-"
     "B/VII.0    [✓]: ENDF-B/VII.0 based library converted from ACE files distr"
+    "ibuted with MCNP5/6\nlanl/endfb71     ENDF-B/VII.1    [ ]: ENDF-B/VII.1 b"
+    "ased library converted from ACE files distributed with MCNP5/6\nlanl/endf"
+    "b8      ENDF-B.VIII.0   [ ]: ENDF-B/VIII.0 based library converted from A"
+    "CE files distributed by Los Alamos National lab (LANL)\n-----------------"
+    "------------------------------------------------  Custom Libraries  -----"
+    "------------------------------------------------------------\n")
+    assert captured.out == expected
+
+    run("remove lanl/endfb70")
+    run("list")
+    captured = capsys.readouterr()
+    expected = ("-------------------------------------------------------------"
+    "-  Installable Libraries  -----------------------------------------------"
+    "----------------\nofficial/endfb71 ENDF-B/VII.1    [ ]: Official OpenMC l"
+    "ibrary based on ENDF-B/VII.1\nofficial/endfb8  ENDF-B/VIII.0   [ ]: Offic"
+    "ial OpenMC library based on ENDF-B/VIII.0\nofficial/jeff33  JEFF-3.3     "
+    "   [ ]: Official OpenMC library based on JEFF-3.3\nlanl/endfb70     ENDF-"
+    "B/VII.0    [ ]: ENDF-B/VII.0 based library converted from ACE files distr"
     "ibuted with MCNP5/6\nlanl/endfb71     ENDF-B/VII.1    [ ]: ENDF-B/VII.1 b"
     "ased library converted from ACE files distributed with MCNP5/6\nlanl/endf"
     "b8      ENDF-B.VIII.0   [ ]: ENDF-B/VIII.0 based library converted from A"
