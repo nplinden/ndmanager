48ba07694e20371088e2f78a43ee7fe75144aeda
Nicolas Linden <n.linde@naarea.fr>
2024-11-27 18:26:07 +0100

    change CI ?

--- tests/CLI/test_ndc.py
+++ tests/CLI/test_ndc.py
@@ -1,6 +1,9 @@
 import shlex
 import pytest
+from pathlib import Path
+import argparse as ap
 from ndmanager.CLI.chainer.main import parser
+from ndmanager.CLI.chainer.build import NdcBuildCommand
 
 def run(command):
     args = parser.parse_args(shlex.split(command))
@@ -63,3 +66,23 @@ def test_ndc_list_install_remove(capsys):
     "----------------  Available Chains  --------------------------------------"
     "---------------------------\n\n")
     assert captured.out == expected
+
+#     data = """name: jeff33-fast
+# description: |
+#   A depletion chain based on the JEFF-3.3 evaluations.
+# branching_ratios: sfr
+# n:
+#   base: jeff33
+#   ommit: C0
+#   add:
+#     endfb8: Ta180 C12 C13 O17
+# decay: 
+#   base: jeff33
+# nfpy:
+#   base: jeff33
+# """
+#     p = Path("pytest-artifacts/chain-test.yml")
+#     with open(p, "w") as f:
+#         print(data, file=f)
+#     namespace = ap.Namespace(filename=str(p))
+#     NdcBuildCommand(namespace)
