19df0aebbc4a252f1219258ab04c9aba12ffebb6
Nicolas Linden <n.linde@naarea.fr>
2024-11-27 09:38:46 +0100

    fix ?

--- tests/CLI/test_ndo.py
+++ tests/CLI/test_ndo.py
@@ -46,7 +46,25 @@ def test_ndo_list_install_remove(capsys):
     "------------------------------------------------------------\n")
     assert captured.out == expected
 
-    run("remove lanl/endfb70")
+    run("clone lanl/endfb70 coucou")
+    run("list")
+    captured = capsys.readouterr()
+    expected = ("-------------------------------------------------------------"
+    "-  Installable Libraries  -----------------------------------------------"
+    "----------------\nofficial/endfb71 ENDF-B/VII.1    [ ]: Official OpenMC l"
+    "ibrary based on ENDF-B/VII.1\nofficial/endfb8  ENDF-B/VIII.0   [ ]: Offic"
+    "ial OpenMC library based on ENDF-B/VIII.0\nofficial/jeff33  JEFF-3.3     "
+    "   [ ]: Official OpenMC library based on JEFF-3.3\nlanl/endfb70     ENDF-"
+    "B/VII.0    [✓]: ENDF-B/VII.0 based library converted from ACE files distr"
+    "ibuted with MCNP5/6\nlanl/endfb71     ENDF-B/VII.1    [ ]: ENDF-B/VII.1 b"
+    "ased library converted from ACE files distributed with MCNP5/6\nlanl/endf"
+    "b8      ENDF-B.VIII.0   [ ]: ENDF-B/VIII.0 based library converted from A"
+    "CE files distributed by Los Alamos National lab (LANL)\n-----------------"
+    "------------------------------------------------  Custom Libraries  -----"
+    "------------------------------------------------------------\ncoucou\n")
+    assert captured.out == expected
+
+    run("remove lanl/endfb70 coucou")
     run("list")
     captured = capsys.readouterr()
     expected = ("-------------------------------------------------------------"
