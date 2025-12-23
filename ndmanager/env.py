6d0eb6055b25a1403e67fc99ce1bdd2129d004a3
Nicolas Linden <nicolas@nlinden.com>
2025-12-23 16:54:59 +0100

    revamp env variables

--- ndmanager/env.py
+++ ndmanager/env.py
@@ -5,50 +5,21 @@ libraries
 import os
 from pathlib import Path
 
-import yaml
-
-if "NDMANAGER_CONFIG" in os.environ:
-    NDMANAGER_CONFIG = Path(os.environ["NDMANAGER_CONFIG"]).absolute()
-else:
-    NDMANAGER_CONFIG = Path.home() / ".config/ndmanager/"
-
-if (settings_path := NDMANAGER_CONFIG / "settings.yml").exists():
-    with open(settings_path, "r", encoding="utf-8") as f:
-        settings = yaml.safe_load(f)
-else:
-    settings = {}
-
 if "NDMANAGER_ENDF6" in os.environ:
     NDMANAGER_ENDF6 = Path(os.environ["NDMANAGER_ENDF6"]).absolute()
-elif "NDMANAGER_ENDF6" in settings:
-    NDMANAGER_ENDF6 = Path(settings["NDMANAGER_ENDF6"]).absolute()
 else:
-    NDMANAGER_ENDF6 = NDMANAGER_CONFIG / "endf6"
+    NDMANAGER_ENDF6 = Path.home() / "ndmanager/endf6"
 
 if "NDMANAGER_HDF5" in os.environ:
     NDMANAGER_HDF5 = Path(os.environ["NDMANAGER_HDF5"]).absolute()
-elif "NDMANAGER_HDF5" in settings:
-    NDMANAGER_HDF5 = Path(settings["NDMANAGER_HDF5"]).absolute()
 else:
-    NDMANAGER_HDF5 = NDMANAGER_CONFIG / "hdf5"
+    NDMANAGER_HDF5 = Path.home() / "ndmanager/hdf5"
 
 if "NDMANAGER_CHAINS" in os.environ:
     NDMANAGER_CHAINS = Path(os.environ["NDMANAGER_CHAINS"]).absolute()
-elif "NDMANAGER_CHAINS" in settings:
-    NDMANAGER_CHAINS = Path(settings["NDMANAGER_CHAINS"]).absolute()
 else:
-    NDMANAGER_CHAINS = NDMANAGER_CONFIG / "chains"
+    NDMANAGER_CHAINS = Path.home() / "ndmanager/chains"
 
-if "NDMANAGER_SAMPLES" in os.environ:
-    NDMANAGER_SAMPLES = Path(os.environ["NDMANAGER_SAMPLES"]).absolute()
-elif "NDMANAGER_SAMPLES" in settings:
-    NDMANAGER_SAMPLES = Path(settings["NDMANAGER_SAMPLES"]).absolute()
-else:
-    NDMANAGER_SAMPLES = NDMANAGER_CONFIG / "samples"
-
-if "NDMANAGER_COV" in os.environ:
-    NDMANAGER_COV = Path(os.environ["NDMANAGER_COV"]).absolute()
-elif "NDMANAGER_COV" in settings:
-    NDMANAGER_COV = Path(settings["NDMANAGER_COV"]).absolute()
-else:
-    NDMANAGER_COV = NDMANAGER_CONFIG / "cov"
+NDMANAGER_ENDF6.mkdir(parents=True, exist_ok=True)
+NDMANAGER_HDF5.mkdir(parents=True, exist_ok=True)
+NDMANAGER_CHAINS.mkdir(parents=True, exist_ok=True)
